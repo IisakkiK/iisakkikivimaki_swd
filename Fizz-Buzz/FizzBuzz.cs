13df9dbcdc694b60b62f2a223e907ee942bfb1c0
IisakkiK <iisakki.kivimaki@gmail.com>
2020-10-27 16:22:39 +0200

    FizzBuzz Valmis
    Valmis tehtävä

---
+++ Fizz-Buzz/FizzBuzz.cs
@@ -0,0 +1,43 @@
+﻿namespace Fizz_Buzz
+{
+    public class FizzBuzz
+    {
+        public FizzBuzz()
+        {
+        }
+
+        public static string Luku(int syote)
+        {
+            if (OnkoJaollinenViidellaToista(syote))
+            {
+                return "FizzBuzz";
+            }
+            if (OnkoJaollinenKolmella(syote))
+            {
+                return "Fizz";
+            }
+
+            if (OnkoJaollinenViidella(syote))
+            {
+                return "Buzz";
+            }
+
+            return syote.ToString();
+        }
+
+        private static bool OnkoJaollinenViidellaToista(int syote)
+        {
+            return OnkoJaollinenKolmella(syote) && OnkoJaollinenViidella(syote);
+        }
+
+        private static bool OnkoJaollinenKolmella(int syote)
+        {
+            return syote % 3 == 0;
+        }
+
+        private static bool OnkoJaollinenViidella(int syote)
+        {
+            return syote % 5 == 0;
+        }
+    }
+}
--- Fizz-BuzzTests/FizzBuzzTests.cs
+++ Fizz-BuzzTests/FizzBuzzTests.cs
@@ -1,18 +1,70 @@
+using Fizz_Buzz;
 using NUnit.Framework;
 
 namespace Fizz_BuzzTests
 {
     public class FizzbuzzTests
     {
+        FizzBuzz fizzBuzz = null;
+
         [SetUp]
         public void Setup()
         {
+            fizzBuzz = new FizzBuzz();
         }
 
         [Test]
-        public void Test1()
+        public void LuodaanFizzBuzzLuokka()
         {
-            Assert.Pass();
+            Assert.IsInstanceOf<FizzBuzz>(fizzBuzz);
+        }
+
+        [Test]
+        public void TulostaLukuKunSyotteenaAnnettuYksi()
+        {
+            string vastaus = FizzBuzz.Luku(1);
+
+            Assert.AreEqual("1", vastaus);
+        }
+        [Test]
+        public void TulostaLukuKunSyotteenaAnnettuKaksi()
+        {
+            string vastaus = FizzBuzz.Luku(2);
+
+            Assert.AreEqual("2", vastaus);
+        }
+
+
+        [Test]
+        public void TulostaFizzJosLukuOnKolmellaJaollinen()
+        {
+            string vastaus = FizzBuzz.Luku(3);
+
+            Assert.AreEqual("Fizz", vastaus);
+        }
+
+        [Test]
+        public void TulostaBuzzJosLukuOnViidellaJaollinen()
+        {
+            string vastaus = FizzBuzz.Luku(5);
+
+            Assert.AreEqual("Buzz", vastaus);
+        }
+
+        [Test]
+        public void TulostaLukuKunSyotteenaAnnettuKuusi()
+        {
+            string vastaus = FizzBuzz.Luku(6);
+
+            Assert.AreEqual("Fizz", vastaus);
+        }
+
+        [Test]
+        public void TulostaFizzBuzzJosLukuOnKolmellaJaViidellaJaollinen()
+        {
+            string vastaus = FizzBuzz.Luku(15);
+
+            Assert.AreEqual("FizzBuzz", vastaus);
         }
     }
 }
